fd115095776afe6ac0e0f93a54565fe9157bc09d
Nick Garcia <NicksIdeaEngine@gmail.com>
2020-11-25 11:15:09 -0500

    Can't forget the doggo

---
+++ src/assets/Doggo.js
@@ -0,0 +1,10 @@
+const Doggo = () => {
+  const xsmol = 'https://placedog.net/50/50?random'
+  const smol = 'https://placedog.net/150/150?random'
+  const med = 'https://placedog.net/300/300?random'
+  const lorg = 'https://placedog.net/575/400?random'
+  const xlorg = 'https://placedog.net/900/750?random'
+  return { xsmol, smol, med, lorg, xlorg }
+}
+
+export default Doggo
